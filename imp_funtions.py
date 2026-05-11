b9c87a0694601aed25fcb6f3031bc039daf04f69
Kamil Sayyed <115294719+kamilsayyed26@users.noreply.github.com>
2026-05-11 12:18:11 +0530

    Add files via upload

---
+++ imp_funtions.py
@@ -0,0 +1,14 @@
+def summation(*args):
+	result = 0
+	for i in args:
+		result = result + i
+	return result
+
+def multiply(a,b):
+	"""This is multiply function.
+	This will mutiply a*b"""
+	result = a*b
+	return result
+def div(a,b):
+	result = a/b
+	return result        
